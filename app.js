da9ba5beec1d91784f07a1ce8e13f834193cb896
Hub01 <5546683-hubwork@users.noreply.gitlab.com>
2022-12-16 19:38:57 +0330

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) =>
-  res.send("Node.js local development on Docker container and Nodemon work!")
+  res.send("Node.js local development on Docker container and Nodemon work")
 );
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
